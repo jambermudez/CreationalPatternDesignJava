71ecee97983893711c097ee6eb75e56877d3e043
jambermudez <josemartinbermudez@gmail.com>
2022-01-14 21:09:02 +0100

    Template Main

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("---------------------------------------------------------");
+		//System.err.print(" ???????? ");
+		System.out.print("---------------------------------------------------------");
+		System.out.println();
+		
+		//testPattern();
+
+	}
+	
+//	private static void testPattern() {
+//
+//
+//	}
+	
+
+}
